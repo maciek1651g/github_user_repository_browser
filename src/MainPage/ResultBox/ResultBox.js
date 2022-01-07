dd9289a2721ba9e47177b1b22c26aae69c2d57f0
Maciej Dominiak <maciek1651g@gmail.com>
2022-01-07 11:42:15 +0100

    off pagination

--- src/MainPage/ResultBox/ResultBox.js
+++ src/MainPage/ResultBox/ResultBox.js
@@ -13,11 +13,7 @@ const ResultBox = ({ owner, repos, page, setPage }) => {
         const pageOfRepos = [];
         const numberFirstRepo = (page - 1) * 30;
 
-        for (
-            let i = numberFirstRepo;
-            i < numberFirstRepo + 30 && i < repos.length;
-            i++
-        ) {
+        for (let i = 0; i < repos.length; i++) {
             pageOfRepos.push(<RepoBox key={i + 1} index={i + 1} repo={repos[i]} />);
         }
 
